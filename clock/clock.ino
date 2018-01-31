aa475816dc5028cad185976d8337aa0cc41170e9
kevin578 <kbriggs@aapl000894.local>
2018-01-31 16:04:13 -0500

    updated clock

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,3 +1,9 @@
+#include <LiquidCrystal.h>
+#include <SparkFunDS1307RTC.h>
+#include <Wire.h>
+
+const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,11 +17,17 @@ void loop() {
   // put your main code here, to run repeatedly:
  
   rtc.update();
+  rtc.set12Hour();
   Serial.print(rtc.minute());
   lcd.clear();
+  lcd.print("Current time: ");
+  lcd.setCursor(0,1);
   lcd.print(rtc.hour());
   
   lcd.print(":");
+  if (rtc.minute() < 10) {
+    lcd.print(0);
+  }
   lcd.print(rtc.minute());
   //Serial.print(rtc.hour());
  
